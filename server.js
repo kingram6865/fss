602f565d436b6ca6632921e22f02a9d0d2d30e92
Ken Ingram <ken.ingram@gmail.com>
2021-03-31 11:51:50 -0700

    Adjust for Heroku deployment

--- server.js
+++ server.js
@@ -15,4 +15,5 @@ app.use(cors())
 app.use(logger('dev'))
 app.use('/api', schedule)
 
-app.listen(PORT, SERVER, () => console.log(`Listening on port: ${PORT} at ${SERVER}`))
+// app.listen(PORT, SERVER, () => console.log(`Listening on port: ${PORT} at ${SERVER}`))
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
