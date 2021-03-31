450d82e70c0262043870f4747769f42e4569ec24
Ken Ingram <ken.ingram@gmail.com>
2021-03-31 12:37:39 -0700

    Add ssl={rejectUnauthorized: false}

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,13 @@ const pg = require('pg-promise')()
 // const cn = `postgres://${process.env.PGDB_USER}:${process.env.PGDB_PW}@${process.env.PG_HOST}:${process.env.PGDB_PORT}/${process.env.DB}`
 // const cn = `postgres://${process.env.HPGDB_USER}:${process.env.HPGDB_PW}@${process.env.HPG_HOST}:${process.env.HPGDB_PORT}/${process.env.HPG_DB}?sslmode=require`
 const cn = `${process.env.DATABASE_URL}?sslmode=require`
-const db = pg(cn)
+const ssl = { rejectUnauthorized: false }
+const config = {
+  connectionString: `${cn}`,
+  ssl: ssl
+}
+
+const db = pg(config)
 
 const DBLOGIN = {
   host: process.env.DB_HOST,
