cdacde37f7ac06fdeea39700f8dbfa99000de94a
Ken Ingram <ken.ingram@gmail.com>
2021-04-02 17:49:31 -0700

    Completed editAppointment configuration

--- controllers/schedule.js
+++ controllers/schedule.js
@@ -1,14 +1,21 @@
 const { db } = require('../db/connection')
 
-async function executeSQL (sql) {
-  const results = await db.any(sql)
-  return results  
-}
+// async function executeSQL (sql, params = null) {
+//   console.log(sql, params)
+//   if (params) {
+//     const results = await db.any(sql, params, (err) => {
+//       console.log(params)
+//       if (err) throw err
+//     })
+//   }
+//   const results = await db.any(sql)
+//   return results  
+// }
 
 async function getAppointments (req, res) {
   try {
-    const SQL = `SELECT * FROM appointment_schedule ORDER BY `
-    const rows = await executeSQL(SQL)
+    const SQL = `SELECT * FROM appointment_schedule ORDER BY`
+    const rows = await db.any(SQL)
     res.json(rows)
   } catch (error) {
     res.status(500).json(error.message)
@@ -18,7 +25,7 @@ async function getAppointments (req, res) {
 async function getAppointment (req, res) {
   try {
     const SQL = `SELECT * FROM appointment_schedule WHERE objid = ${req.params.id}`
-    const rows = await executeSQL(SQL)
+    const rows = await db.any(SQL)
     res.json(rows)
   } catch (error) {
     res.status(500).json(error.message)
@@ -26,6 +33,14 @@ async function getAppointment (req, res) {
 }
 
 async function addAppointment (req, res) {
+  const values = [
+    req.params.lessonid,
+    req.params.timeid,
+    req.params.teacherid,
+    req.params.clientid,
+    req.params.confirmed
+  ]
+
   try {
     const SQL = `INSERT INTO appointment_schedule (
         lesson_detail_id, 
@@ -33,18 +48,10 @@ async function addAppointment (req, res) {
         teacher_id, 
         client_id, 
         confirmed
-      )
-      VALUES (
-        ${req.params.lesson_id},
-        ${req.params.time_increment_id},
-        ${req.params.instructor_id},
-        ${req.params.client_id},
-        ${req.params.confirmed}
-      )`
-    const appointment = await executeSQL(SQL)
+      ) VALUES ($1, $2, $3, $4, $5)`
+    const appointment = await db.any(SQL. values)
     res.status(201).json(appointment)
   } catch (error) {
-    // errMsg(error.message)
     res.status(500).json(error.message)
   }
 }
@@ -60,16 +67,16 @@ async function editAppointment (req, res) {
 
   try {
     const SQL = `UPDATE appointment_schedule 
-    SET lesson_detail_id = ?,
-    time_increment_id = ?,
-    teacher_id = ?,
-    client_id = ?,
-    confirmed = ?
+    SET lesson_detail_id = $1,
+    time_increment_id = $2,
+    instructor_id = $3,
+    client_id = $4,
+    confirmed = $5,
+    last_edit = NOW()
     WHERE objid = ${req.params.id}`
-    const appointment = await executeSQL(SQL, values)
+    const appointment = await db.any(SQL, values)
     res.status(201).json(appointment)
   } catch (error) {
-    // errMsg(error.message)
     res.status(500).json(error.message)
   }
 }
@@ -77,27 +84,53 @@ async function editAppointment (req, res) {
 async function deleteAppointment (req, res) {
   try {
     const SQL = `DELETE FROM appointment_schedule WHERE objid = ${req.params.id}`
-    const appointment = await executeSQL(SQL)
+    const appointment = await db.any(SQL)
   } catch (error) {
     res.status(500).json(error.message)
-    // errMsg(error.message)
   }
 }
 
-// async function test () {
+async function test () {
+  try {
+    const SQL = `SELECT * FROM appointment_schedule`
+    const rows = await db.any(SQL)
+    rows.forEach(x => {
+      console.log(`Line 107: ${x.created} ${x.objid} ${x.start_time} ${x.end_time} ${x.instructor} ${x.client} ${x.confirmed} ${x.last_edit}`)
+    })
+  } catch (error) {
+    console.log(error.message)
+  } finally {
+    // conn.end()
+    db.$pool.end()
+  }
+}
+
+test()
+
+// async function editTest (arr) {
+//   const req = {
+//     params: {id: 23}
+//   }
+//   const values = [...arr]
+
 //   try {
-//     const SQL = `SELECT * FROM appointment_schedule`
-//     const [rows, fields] = await executeSQL(SQL)
-//     console.log(rows)
+//     const SQL = `UPDATE appointment_schedule 
+//     SET lesson_detail_id = $1,
+//     time_increment_id = $2,
+//     instructor_id = $3,
+//     client_id = $4,
+//     confirmed = $5,
+//     last_edit = NOW()
+//     WHERE objid = ${req.params.id}`
+//     const appointment = await db.any(SQL, values)
+//     console.log(appointment)
 //   } catch (error) {
+//     // errMsg(error.message)
 //     console.log(error.message)
-//   } finally {
-//     // conn.end()
-//     db.$pool.end()
 //   }
 // }
 
-// test()
+// editTest([2, 13, 9, 20, 1])
 
 module.exports = {
   getAppointments,
