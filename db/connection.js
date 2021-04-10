7fa472717116b8d678a330e0d5add78f4ea22d7e
Ken Ingram <ken.ingram@gmail.com>
2021-04-09 21:00:51 -0700

    Update remaining changes.

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,8 @@ require('dotenv').config({
 const pg = require('pg-promise')()
 // const cn = `postgres://${process.env.PGDB_USER}:${process.env.PGDB_PW}@${process.env.PG_HOST}:${process.env.PGDB_PORT}/${process.env.DB}`
 // const cn = `postgres://${process.env.HPGDB_USER}:${process.env.HPGDB_PW}@${process.env.HPG_HOST}:${process.env.HPGDB_PORT}/${process.env.HPG_DB}`
-const cn = `${process.env.DATABASE_URL}`
+// const cn = `${process.env.DATABASE_URL}`
+const cn = `${process.env.DB_DATA}`
 const ssl = { rejectUnauthorized: false }
 const config = {
   connectionString: `${cn}`,
